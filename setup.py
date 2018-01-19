1472a6ac706a7154972ef2889eb9c0999e796b56
Tero Koskinen <tero.koskinen@iki.fi>
2018-01-19 20:43:57 +0200

    Tweak setup.py a little.

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@
 from __future__ import with_statement
 
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
 
 long_desc = '''
 This package contains the adadomain Sphinx extension.
@@ -10,12 +14,8 @@ This package contains the adadomain Sphinx extension.
 
 requires = ['Sphinx>=1.0']
 
-def readme():
-    try:
-        with open('README.rst') as f:
-            return f.read()
-    except IOError:
-        pass
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='sphinxcontrib-adadomain',
@@ -26,7 +26,7 @@ setup(
     author='Tero Koskinen',
     author_email='tero.koskinen@iki.fi',
     description='Sphinx "adadomain" extension',
-    long_description=readme(),
+    long_description=long_description,
     zip_safe=False,
     classifiers=[
         'Development Status :: 4 - Beta',
